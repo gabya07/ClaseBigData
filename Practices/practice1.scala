3af1d7acce680ab30cbe6c026cf2b6b86a303938
gabya07 <codemx.info@gmail.com>
2022-09-07 20:15:53 -0700

    Ejercicio 2

--- Practices/practice1.scala
+++ Practices/practice1.scala
@@ -3,3 +3,10 @@
 val Diameter: Int = 20
 val Radio: Int = Diameter / 2
 
+//Desarrollar un algoritmo en scala que me diga si un numero es primo
+var num = 7
+if (num % 2 == 0) {
+    printf("No es número primo")
+} else {
+    printf("Si es número primo")
+}
