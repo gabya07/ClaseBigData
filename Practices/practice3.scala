797b22cec5775cdd1597805414c812bb5ca53739
chavezRC <chavez.ricardo@uabc.edu.mx>
2022-09-14 19:39:31 -0700

    Funcion que valida numeros odd or EVEN

--- Practices/practice3.scala
+++ Practices/practice3.scala
@@ -6,21 +6,32 @@ def isEven(num:Int): num%2 == 0
     println(isEven(3))
 //Practice 3, analyse the following code with your own words
 
-def listEvens(list:List[Int]): String ={
-    for(n <- list){
-        if(n%2==0){
-            println(s"$n is even")
-        }else{
-            println(s"$n is odd")
+def isEven(num:Int): Boolean = {
+     return num%2 == 0
+ }
+def isEven(num:Int): num%2 == 0
+    println(isEven(6))
+    println(isEven(3))
+//Practice 3, analyse the following code with your own words
+
+// This function takes a list, validates and prints if the numbers within the list are ODD or EVEN, 
+//at the very end it will print DONE
+
+def listEvens(list:List[Int]): String ={ // define una func listEvents tipo INT que va a regresar un STRING
+    for(n <- list){                     // ciclo FOR para recorrer los valores en la lista 
+        if(n%2==0){                     // if para determinar que un numero sera odd o even con MOD %
+            println(s"$n is even")      // si el sobrante de MOD es 0 se imprime el numero y texto que indica que es EVEN
+        }else{                          // si no es asi    
+            println(s"$n is odd")       //si el sobrante de MOD NO es 0 se imprime el numero y texto que indica que es ODD
         }
     }
-    return "Done"
+    return "Done"                       // imprime done al terminar de recorrer la lista
 }
 
-val l = List(1,2,3,4,5,6,7,8)
-val l2 = List(4,3,22,55,7,8)
-listEvens(l)
-listEvens(l2)
+val l = List(1,2,3,4,5,6,7,8)           //Se crea una lista (1) con los valores predeterminados
+val l2 = List(4,3,22,55,7,8)            //Se crea una lista con los valores predeterminados
+listEvens(l)                            //Se llama la funcion ListEvens previamente creada con la list 1
+listEvens(l2)                           ////Se llama la funcion ListEvens previamente creada con la list 12
 
 //3 7 afortunado
 
