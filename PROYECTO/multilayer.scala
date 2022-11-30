fc9a92a1ab5dbfe3b8e82a3fc1b085dc05c7e430
gabya07 <codemx.info@gmail.com>
2022-11-30 15:50:43 -0800

    Adding both methods

---
+++ PROYECTO/multilayer.scala
@@ -0,0 +1,43 @@
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.IndexToString
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.Pipeline
+//Importar librerias utilizables con el modelo multilayer
+//Y los indexadores para clasificar
+
+//Importar datos desde el csv, limpieza de datos, cambiamos var data a bank-full por el infer schema
+var bank = spark.read.option("header","true").option("inferSchema", "true").format("csv").load("bank-full.csv")
+
+//Nombres de las columnas
+bank.show(0)
+
+//Ver cómo es el esquema
+bank.printSchema()
+
+//Usa el método describe () para aprender más sobre los datos del DataFrame.
+bank.describe().show()
+
+//Haga la transformación pertinente para los datos categóricos los cuales serán nuestras etiquetas a clasificar.
+val assembler = new VectorAssembler().setInputCols(Array("age","job","marital","education","default","balance","housing","loan","contact","day","month","duration","campaign","pdays","previous","poutcome")).setOutputCol("features")
+//Total las caracteristicas
+val features = assembler.transform(bank)
+//Agarra un dato categorico y lo vuelve numerico
+val indexerLabel = new StringIndexer().setInputCol("y").setOutputCol("indexedLabel").fit(features)
+//val indexerFeatures = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(16)
+//val Array(training, test) = features.randomSplit(Array(0.7, 0.3), seed = 41234)
+
+//Construir el modelo de clasificación y explique su arquitectura.
+//val layers = Array[Int](16, 5, 4, 3, 2)
+//val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setBlockSize(128).setSeed(1234).setMaxIter(100)
+//val converterLabel = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(indexerLabel.labels)
+//val pipeline = new Pipeline().setStages(Array(indexerLabel, indexerFeatures, trainer, converterLabel))
+//val model = pipeline.fit(training)
+
+//Resultados del modelo y de sus observaciones.
+//val results = model.transform(test)
+//val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+//val accuracy = evaluator.evaluate(results)
+//println("Error = " + (1.0 - accuracy))
