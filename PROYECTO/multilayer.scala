39faff2a47c8ee971d70a69ebd9f064babb76d42
gabya07 <codemx.info@gmail.com>
2022-11-30 23:27:12 -0800

    .88 Multilayer

--- PROYECTO/multilayer.scala
+++ PROYECTO/multilayer.scala
@@ -4,7 +4,6 @@ import org.apache.spark.ml.feature.IndexToString
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.feature.VectorIndexer
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.OneHotEncoder
 import org.apache.spark.ml.Pipeline
 
 //Importar librerias utilizables con el modelo multilayer
@@ -31,39 +30,36 @@ val contactIndexer = new StringIndexer().setInputCol("contact").setOutputCol("co
 val monthIndexer = new StringIndexer().setInputCol("month").setOutputCol("monthIndex")
 val poutcomeIndexer = new StringIndexer().setInputCol("poutcome").setOutputCol("poutcomeIndex")
 
-// Transform string type columns to string indexer 
-val jobEncoder = new OneHotEncoder().setInputCol("jobIndex").setOutputCol("jobVec")
-val educationEncoder = new OneHotEncoder().setInputCol("educationIndex").setOutputCol("educationVec")
-val maritalEncoder = new OneHotEncoder().setInputCol("maritalIndex").setOutputCol("maritalVec")
-val defaultEncoder = new OneHotEncoder().setInputCol("defaultIndex").setOutputCol("defaultVec")
-val housingEncoder = new OneHotEncoder().setInputCol("housingIndex").setOutputCol("housingVec")
-val loanEncoder = new OneHotEncoder().setInputCol("loanIndex").setOutputCol("loanVec")
-val contactEncoder = new OneHotEncoder().setInputCol("contactIndex").setOutputCol("contactVec")
-val monthEncoder = new OneHotEncoder().setInputCol("monthIndex").setOutputCol("monthVec")
-val poutcomeEncoder = new OneHotEncoder().setInputCol("poutcomeIndex").setOutputCol("poutcomeVec")
-
-
+val indexed = educationIndexer.fit(bank).transform(bank)
+val indexed1 = jobIndexer.fit(indexed).transform(indexed)
+val indexed2 = maritalIndexer.fit(indexed1).transform(indexed1)
+val indexed3 = defaultIndexer.fit(indexed2).transform(indexed2)
+val indexed4 = housingIndexer.fit(indexed3).transform(indexed3)
+val indexed5 = loanIndexer.fit(indexed4).transform(indexed4)
+val indexed6 = contactIndexer.fit(indexed5).transform(indexed5)
+val indexed7 = monthIndexer.fit(indexed6).transform(indexed6)
+val indexed8 = poutcomeIndexer.fit(indexed7).transform(indexed7)
+//delete columns
+indexed8.drop("job","marital","education","default","housing","loan","contact","month", "poutcome").show(false)
 // Assemble everything together to be ("label","features") format
 //Haga la transformación pertinente para los datos categóricos los cuales serán nuestras etiquetas a clasificar.
-//val assembler = new VectorAssembler().setInputCols(Array("age","job","marital","education","default","balance","housing","loan","contact","day","month","duration","campaign","pdays","previous","poutcome")).setOutputCol("features")
-val assembler = new VectorAssembler().setInputCols(Array("age","jobVec","maritalVec","educationVec","defaultVec","balance","housingVec","loanVec","contactVec","day","monthVec","duration","campaign","pdays","previous","poutcomeVec")).setOutputCol("features")
+val assembler = new VectorAssembler().setInputCols(Array("age","jobIndex","maritalIndex","educationIndex","defaultIndex","balance","housingIndex","loanIndex","contactIndex","day","monthIndex","duration","campaign","pdays","previous","poutcomeIndex")).setOutputCol("features")
+//val assembler = new VectorAssembler().setInputCols(Array("age","jobVec","maritalVec","educationVec","defaultVec","balance","housingVec","loanVec","contactVec","day","monthVec","duration","campaign","pdays","previous","poutcomeVec")).setOutputCol("features")
 
-val features = assembler.transform(bank)
-//Agarra un dato categorico y lo vuelve numerico
+val features = assembler.transform(indexed8)
+// Agarra un dato categorico y lo vuelve numerico
 val indexerLabel = new StringIndexer().setInputCol("y").setOutputCol("indexedLabel").fit(features)
-
-val indexerFeatures = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(6)
-
+val indexerFeatures = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(16)
 val Array(training, test) = features.randomSplit(Array(0.7, 0.3), seed = 12345)
 
-//Construir el modelo de clasificación y explique su arquitectura.
-val layers = Array[Int](16, 5, 3, 2)
-val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setBlockSize(40000).setSeed(12345).setMaxIter(100)
+// //Construir el modelo de clasificación y explique su arquitectura.
+val layers = Array[Int](16,7, 2)
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setBlockSize(12345).setSeed(1234).setMaxIter(1234)
 val converterLabel = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(indexerLabel.labels)
 val pipeline = new Pipeline().setStages(Array(indexerLabel, indexerFeatures, trainer, converterLabel))
 val model = pipeline.fit(training)
 
-//Resultados del modelo y de sus observaciones.
+// //Resultados del modelo y de sus observaciones.
 val results = model.transform(test)
 val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
 val accuracy = evaluator.evaluate(results)
